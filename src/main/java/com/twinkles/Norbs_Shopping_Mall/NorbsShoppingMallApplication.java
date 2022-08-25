898cd8bc89ed7c8789402b4c090ecaf1ae3942f3
Adewale Adeyinka <adeyinkawale13@gmail.com>
2022-08-25 07:18:16 +0100

    "Added pom file"

---
+++ src/main/java/com/twinkles/Norbs_Shopping_Mall/NorbsShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package com.twinkles.Norbs_Shopping_Mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorbsShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorbsShoppingMallApplication.class, args);
+	}
+
+}
